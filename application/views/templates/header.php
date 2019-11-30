a861d26ddc19d8f67ef41540fa1979fb8dd8a53a
rhexy ilham madany <rehexy_173040125@mail.unpas.ac.id>
2019-11-30 17:06:58 +0700

    Tampilan awal menggunakan frame work Ccodeigniter

---
+++ application/views/templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>MyWatch</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="The Plaza eCommerce Template">
+	<meta name="keywords" content="plaza, eCommerce, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="<?=base_url(); ?>assets/img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/css/owl.carousel.css"/>
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/css/style.css"/>
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/css/animate.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
---
+++ application/views/templates/menubar.php
@@ -0,0 +1,31 @@
+<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+	
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="container-fluid">
+			<!-- logo -->
+			<div class="site-logo">
+				<img src="<?=base_url(); ?>assets/img/logo.png" alt="logo">
+			</div>
+			<!-- responsive -->
+			<div class="nav-switch">
+				<i class="fa fa-bars"></i>
+			</div>
+			<div class="header-right">
+				<a href="<?= base_url('keranjang/index');  ?>" class="card-bag"><img src="<?=base_url(); ?>assets/img/icons/bag.png" alt=""><span>2</span></a>
+				<a href="#" class="search"><img src="<?=base_url(); ?>assets/img/icons/search.png" alt=""></a>
+			</div>
+			<!-- site menu -->
+			<ul class="main-menu">
+				<li><a href="<?= base_url('home/index');  ?>">Home</a></li>
+				<li><a href="#">Woman</a></li>
+				<li><a href="#">Man</a></li>
+				<li><a href="#">Couple</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="<?= base_url('contact/index');  ?>">Contact</a></li>
+			</ul>
+		</div>
+	</header>
